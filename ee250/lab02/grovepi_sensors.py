d4b0990ed62e9b3f4a41dcc7ac04105cfb35ca92
Larry Wang <larrywan@usc.edu>
2021-02-04 16:44:14 -0800

    Plz

--- ee250/lab02/grovepi_sensors.py
+++ ee250/lab02/grovepi_sensors.py
@@ -37,13 +37,13 @@ PORT = 4
 grovepi.pinMode(potentiometer,"INPUT")
 time.sleep(1)
 
-	while True:
-        #So we do not poll the sensors too quickly which may introduce noise,
-        #sleep for a reasonable time of 200ms between each iteration.
-    	time.sleep(0.2)
+while True:
+    #So we do not poll the sensors too quickly which may introduce noise,
+    #sleep for a reasonable time of 200ms between each iteration.
+    time.sleep(0.2)
 
-        p = grovepi.analogRead(potentiometer)
-        s = grovepi.ultrasonicRead(PORT))
+    p = grovepi.analogRead(potentiometer)
+    s = grovepi.ultrasonicRead(PORT))
 
-		if(p>s)
+	if(p>s)
 
